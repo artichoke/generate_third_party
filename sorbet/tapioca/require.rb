68e7ccbe90e5141102253e08213f7a76b6d98b83
Ryan Lopopolo <rjl@hyperbo.la>
2022-09-08 21:03:14 -0700

    Run tapioca require

--- sorbet/tapioca/require.rb
+++ sorbet/tapioca/require.rb
@@ -1,4 +1,6 @@
 # typed: true
 # frozen_string_literal: true
 
-# Add your extra requires here (`bin/tapioca require` can be used to boostrap this list)
+require "open3"
+require "stringio"
+require "yaml"
